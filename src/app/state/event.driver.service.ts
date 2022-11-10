03c58792793d013483262d7858fa83aaf1879cdf
nawel ben nejma <nawelbennejma@yahoo.fr>
2022-11-10 13:57:56 +0100

    decomposition1

---
+++ src/app/state/event.driver.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { ActionEvent } from './product.state';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class EventDriverService {
+
+    sourceEventSubject: Subject<ActionEvent> = new Subject<ActionEvent>();
+
+    sourceEventSubjectObservable = this.sourceEventSubject.asObservable();
+
+    constructor() { }
+
+    publishEvent(event:ActionEvent){
+        this.sourceEventSubject.next(event);
+
+    }
+
+}
